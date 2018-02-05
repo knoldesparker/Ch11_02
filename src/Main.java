6b04d711d9240a467560866c2ce41642d979fd16
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-05 13:53:44 +0100

    setting up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
+    public static List<Integer> alternate (List<Integer> list1, List<Integer> list2){
+        List<Integer> list = new LinkedList<Integer>();
+        
+
+
 }
