90ea960262af94d99dccd5f02c110e94d261c6be
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-05 17:24:12 +0100

    Made the mehod

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+       
     }
-    public static List<Integer> alternate (List<Integer> list1, List<Integer> list2){
-        List<Integer> list = new LinkedList<Integer>();
-        
 
+    public static List<Integer> alternate (List<Integer> list1, List<Integer> list2) {
+        List<Integer> list = new LinkedList<>();
+        Iterator<Integer> integerIterator1 = list1.iterator();
+        Iterator<Integer> integerIteratorr2 = list2.iterator();
 
+        while (integerIterator1.hasNext() || integerIteratorr2.hasNext())
+        {
+            if (integerIterator1.hasNext()){
+                list.add(integerIterator1.next());
+            }
+            if (integerIteratorr2.hasNext()){
+                list.add(integerIteratorr2.next());
+            }
+        }
+        return list;
+    }
 }
